aeb16a157ef7c23db162bc7ccddd8ef18cdd1a25
awang1 <awang1@andrew.cmu.edu>
2015-12-05 00:25:00 -0500

    jk now include the initialize.sql file

--- initialize.sql
+++ initialize.sql
@@ -1,1 +1,26 @@
-\copy Users(username, type, password) FROM 'data/Users.txt' DELIMITER ',' CSV
+\copy Users(username, type, password)
+FROM 'data/Users.txt' DELIMITER ',' CSV
+
+\copy Users(username, type, password)
+FROM 'data/PaymentTypes.txt' DELIMITER ',' CSV
+
+\copy Users(username, type, password)
+FROM 'data/Paypals.txt' DELIMITER ',' CSV
+
+\copy Users(username, type, password)
+FROM 'data/CreditCards.txt' DELIMITER ',' CSV
+
+\copy Users(username, type, password)
+FROM 'data/CustomerServiceSpecialists.txt' DELIMITER ',' CSV
+
+\copy Users(username, type, password)
+FROM 'data/Riders.txt' DELIMITER ',' CSV
+
+\copy Users(username, type, password)
+FROM 'data/Requests.txt' DELIMITER ',' CSV
+
+\copy Users(username, type, password)
+FROM 'data/Drivers.txt' DELIMITER ',' CSV
+
+\copy Users(username, type, password)
+FROM 'data/Rides.txt' DELIMITER ',' CSV
