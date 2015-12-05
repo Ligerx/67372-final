93f4dcc6fea3a3b5c38b37fd59915d057b08f6c7
awang1 <awang1@andrew.cmu.edu>
2015-12-04 20:54:43 -0500

    Initial create and initialize files. For some reason git does not want me adding them

---
+++ create.sql
@@ -0,0 +1,85 @@
+\c postgres
+DROP DATABASE IF EXISTS db_project;
+
+CREATE database db_project;
+\c db_project
+
+CREATE TABLE Users (
+   username text PRIMARY KEY,
+   type text,
+   password text
+);
+
+CREATE TABLE PaymentTypes (
+   id serial PRIMARY KEY,
+   payment_type text,
+   username text REFERENCES Users(username),
+   time_added TIMESTAMP DEFAULT LOCALTIMESTAMP(0),
+   time_last_used TIMESTAMP DEFAULT LOCALTIMESTAMP(0),
+   is_primary_payment_method boolean
+);
+
+CREATE TABLE Paypals (
+   id serial PRIMARY KEY,
+   paypal_num text
+);
+
+CREATE TABLE CreditCards (
+   id serial PRIMARY KEY,
+   credit_card_num text,
+   credit_card_type text
+);
+
+CREATE TABLE CustomerServiceSpecialists (
+   id serial PRIMARY KEY,
+   first_name text,
+   last_name text
+);
+
+
+CREATE TABLE Riders (
+   id serial PRIMARY KEY,
+   first_name text,
+   last_name text,
+   is_in_ride boolean,
+   rating integer,
+   phone_number text
+);
+
+
+CREATE TABLE Requests (
+   id serial PRIMARY KEY,
+   rider_id integer REFERENCES Riders(id),
+   pickup_location text,
+   destination_location text,
+   request_fullfilled boolean,
+   cancelled boolean,
+   surge_multiplier integer,
+   time_requested TIMESTAMP DEFAULT LOCALTIMESTAMP(0)
+);
+
+
+CREATE TABLE Drivers (
+   id serial PRIMARY KEY,
+   first_name text,
+   last_name text,
+   is_turned_on boolean,
+   rating text,
+   phone_number text
+);
+
+
+CREATE TABLE Rides (
+   id serial PRIMARY KEY,
+   request_id integer REFERENCES Requests(id),
+   driver_id integer REFERENCES Drivers(id),
+   price integer,
+   driver_rating text,
+   is_completed boolean,
+   distance text,
+   time TIMESTAMP DEFAULT LOCALTIMESTAMP(0),
+   time_fullfilled TIMESTAMP DEFAULT LOCALTIMESTAMP(0),
+   estimated_time_of_arrival text
+);
+
+
