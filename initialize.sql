7d3c303caf44add228c3fff17e8d9ede0ee09184
awang1 <awang1@andrew.cmu.edu>
2015-12-08 12:16:01 -0500

    Make initialize paths relative (./)

--- initialize.sql
+++ initialize.sql
@@ -1,17 +1,17 @@
-\copy Users FROM 'data/Users.txt' DELIMITER ',' CSV
+\copy Users FROM './data/Users.txt' DELIMITER ',' CSV
 
-\copy PaymentTypes FROM 'data/PaymentTypes.txt' DELIMITER ',' CSV
+\copy PaymentTypes FROM './data/PaymentTypes.txt' DELIMITER ',' CSV
 
-\copy Paypals FROM 'data/Paypals.txt' DELIMITER ',' CSV
+\copy Paypals FROM './data/Paypals.txt' DELIMITER ',' CSV
 
-\copy CreditCards FROM 'data/CreditCards.txt' DELIMITER ',' CSV
+\copy CreditCards FROM './data/CreditCards.txt' DELIMITER ',' CSV
 
-\copy CustomerServiceSpecialists FROM 'data/CustomerServiceSpecialists.txt' DELIMITER ',' CSV
+\copy CustomerServiceSpecialists FROM './data/CustomerServiceSpecialists.txt' DELIMITER ',' CSV
 
-\copy Riders FROM 'data/Riders.txt' DELIMITER ',' CSV
+\copy Riders FROM './data/Riders.txt' DELIMITER ',' CSV
 
-\copy Requests FROM 'data/Requests.txt' DELIMITER ',' CSV
+\copy Requests FROM './data/Requests.txt' DELIMITER ',' CSV
 
-\copy Drivers FROM 'data/Drivers.txt' DELIMITER ',' CSV
+\copy Drivers FROM './data/Drivers.txt' DELIMITER ',' CSV
 
-\copy Rides FROM 'data/Rides.txt' DELIMITER ',' CSV
+\copy Rides FROM './data/Rides.txt' DELIMITER ',' CSV
