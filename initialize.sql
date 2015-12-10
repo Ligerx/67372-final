ddf4bf699e061c8355440628a07922e9913953ce
awang1 <awang1@andrew.cmu.edu>
2015-12-09 23:18:01 -0500

    Update reading in data so that it automatically generates the ids

--- initialize.sql
+++ initialize.sql
@@ -1,19 +1,22 @@
-\copy Users FROM './data/Users.txt' DELIMITER ',' CSV
+\copy Users(username, type, password) FROM './data/Users.txt' DELIMITER ',' CSV
 
-\copy PaymentTypes FROM './data/PaymentTypes.txt' DELIMITER ',' CSV
+-- no primary key field, automatic
+\copy PaymentTypes(username, payment_type, is_primary_payment_method, time_added, time_last_used) FROM './data/PaymentTypes.txt' DELIMITER ',' CSV
 
-\copy Paypals FROM './data/Paypals.txt' DELIMITER ',' CSV
+\copy Paypals(payment_type_id, paypal_num) FROM './data/Paypals.txt' DELIMITER ',' CSV
 
-\copy CreditCards FROM './data/CreditCards.txt' DELIMITER ',' CSV
+\copy CreditCards(payment_type_id, credit_card_num, credit_card_type) FROM './data/CreditCards.txt' DELIMITER ',' CSV
 
-\copy CustomerServiceSpecialists FROM './data/CustomerServiceSpecialists.txt' DELIMITER ',' CSV
+\copy CustomerServiceSpecialists(username, first_name, last_name) FROM './data/CustomerServiceSpecialists.txt' DELIMITER ',' CSV
 
-\copy Riders FROM './data/Riders.txt' DELIMITER ',' CSV
+\copy Riders(username, first_name, last_name, phone_number, rating, is_in_ride) FROM './data/Riders.txt' DELIMITER ',' CSV
 
-\copy Requests FROM './data/Requests.txt' DELIMITER ',' CSV
+-- no primary key field, automatic
+\copy Requests(rider_username, pickup_location, destination_location, time_requested, surge_multiplier, request_fullfilled, cancelled) FROM './data/Requests.txt' DELIMITER ',' CSV
 
-\copy Drivers FROM './data/Drivers.txt' DELIMITER ',' CSV
+\copy Drivers(username, first_name, last_name, phone_number, rating, is_turned_on) FROM './data/Drivers.txt' DELIMITER ',' CSV
 
-\copy Rides FROM './data/Rides.txt' DELIMITER ',' CSV
+-- no primary key field, automatic
+\copy Rides(request_id, driver_username, estimated_time_of_arrival, is_completed) FROM './data/Rides.txt' DELIMITER ',' CSV
 
-\copy RideInformation FROM './data/RideInformation.txt' DELIMITER ',' CSV
+\copy RideInformation(ride_id, distance, price, rider_rating, driver_rating, time, time_fullfilled) FROM './data/RideInformation.txt' DELIMITER ',' CSV
