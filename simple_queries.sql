7d96555853fd341073dc6b29b3099b090c87238d
Ryan Sickles <rsickles@andrew.cmu.edu>
2015-12-09 23:16:10 -0500

    Added two sql files

---
+++ simple_queries.sql
@@ -0,0 +1,31 @@
+\echo -----------------------------------------------------------------
+\echo  * 1. Cancel ride request
+\echo -----------------------------------------------------------------
+\echo
+
+UPDATE Requests
+SET cancelled=true
+WHERE rider_username='alex';
+
+\echo -----------------------------------------------------------------
+\echo  * 2. Get Lowest Rated Rider
+\echo -----------------------------------------------------------------
+\echo
+
+SELECT *
+FROM Drivers
+ORDER BY DESC
+LIMIT 1;
+
+\echo -----------------------------------------------------------------
+\echo  * 3. View Riders Ride History
+\echo -----------------------------------------------------------------
+\echo
+
+SELECT *
+FROM Rides as ride
+INNER JOIN Requests as req
+ON ride.request_id = req.id
+INNER Join Riders as rider
+ON rider.username = req.rider_username
+WHERE req.request_fullfilled = true AND ride.is_completed = true;
