2d90107c4ebdae92f968d7b772f98d5f07edc18a
TechIsCool <jingqiy@andrew.cmu.edu>
2015-12-10 13:15:55 -0500

    add lois's four queries

--- simple_queries.sql
+++ simple_queries.sql
@@ -1,3 +1,46 @@
+
+-- Lois's four queries
+\echo -----------------------------------------------------------------
+\echo * 1.As driver "Ryan Sickles", I am able to turn on uber
+\echo -----------------------------------------------------------------
+\echo 
+
+UPDATE Drivers
+SET is_turned_on=true
+WHERE username='ryan';
+
+\echo -----------------------------------------------------------------
+\echo * 2. As driver "Ryan Sickles", I am able to change my password
+\echo -----------------------------------------------------------------
+
+UPDATE Users
+SET password = 'newPassword'
+WHERE username = 'ryan';
+
+\echo -----------------------------------------------------------------
+\echo * 3. driver end trip
+\echo -----------------------------------------------------------------
+\echo 
+
+UPDATE Riders
+SET is_in_ride=false
+WHERE username='alex';
+
+UPDATE Rides
+SET is_completed = true
+WHERE driver_username='ryan';
+
+\echo -----------------------------------------------------------------
+\echo * 4. As rider "Lois Yang", I am able to request a ride
+\echo -----------------------------------------------------------------
+\echo 
+
+
+INSERT INTO Requests (id, rider_username, pickup_location, destination_location, surge_multiplier, request_fullfilled, cancelled)
+VALUES (7,'lois','5000 Forbes Avenue','1000 Morewood Avenue',1, false, false);
+
+-- Ryan's three queries
+
 \echo -----------------------------------------------------------------
 \echo  * 1. Cancel ride request
 \echo -----------------------------------------------------------------
