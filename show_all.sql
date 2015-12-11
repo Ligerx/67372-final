35e1a6e8387b325f313a4557defc5308b7d1db3a
Ryan Sickles <rsickles@andrew.cmu.edu>
2015-12-10 22:48:00 -0500

    Formatted show all query better

--- show_all.sql
+++ show_all.sql
@@ -2,40 +2,62 @@
 \echo  Show All Data From Tables
 \echo -----------------------------------------------------------------
 \echo
+\echo -----------------------------------------------------------------
+\echo  Show All Data From CreditCard Table
+\echo -----------------------------------------------------------------
+\echo
 SELECT *
 FROM creditcards;
+\echo -----------------------------------------------------------------
+\echo  Show All Data From Customer Service Specialist Table
 \echo -----------------------------------------------------------------
 \echo
 SELECT *
 FROM customerservicespecialists;
 \echo -----------------------------------------------------------------
+\echo  Show All Data From Driver Table
+\echo -----------------------------------------------------------------
 \echo
 SELECT *
 FROM drivers;
+\echo -----------------------------------------------------------------
+\echo  Show All Data From Payment Type Table
 \echo -----------------------------------------------------------------
 \echo
 SELECT *
 FROM paymenttypes;
 \echo -----------------------------------------------------------------
+\echo  Show All Data From Paypal Table
+\echo -----------------------------------------------------------------
 \echo
 SELECT *
 FROM paypals;
+\echo -----------------------------------------------------------------
+\echo  Show All Data From Requests Table
 \echo -----------------------------------------------------------------
 \echo
 SELECT *
 FROM requests;
 \echo -----------------------------------------------------------------
+\echo  Show All Data From Ride Information Table
+\echo -----------------------------------------------------------------
 \echo
 SELECT *
 FROM rideinformation;
+\echo -----------------------------------------------------------------
+\echo  Show All Data From Riders Table
 \echo -----------------------------------------------------------------
 \echo
 SELECT *
 FROM riders;
 \echo -----------------------------------------------------------------
+\echo  Show All Data From Rides Table
+\echo -----------------------------------------------------------------
 \echo
 SELECT *
 FROM rides;
+\echo -----------------------------------------------------------------
+\echo  Show All Data From Users Table
 \echo -----------------------------------------------------------------
 \echo
 SELECT *
